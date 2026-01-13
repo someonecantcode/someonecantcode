e2e92134f4b168cbb61f28a716242e6b54f77dc2
Tea <117409782+someonecantcode@users.noreply.github.com>
2026-01-13 11:52:30 -0800

    my set up

---
+++ install_workspace.ps1
@@ -0,0 +1,32 @@
+# setup-env.ps1
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+
+# install Scoop 
+if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
+    Write-Host "Installing Scoop..."
+    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+} else {
+    Write-Host "Scoop is already installed."
+}
+
+# update scoop
+scoop install git
+scoop update
+
+
+# add buckets
+scoop bucket add extras
+scoop bucket add java
+scoop bucket add r-bucket https://github.com/cderv/r-bucket.git
+
+# install programs
+scoop install openjdk
+scoop install vscode
+
+
+scoop install tinytex
+tlmgr install pgf amsmath amssymb amsthm graphicx csquotes tikz cancel framed
+# music stuff
+
+scoop install ffmpeg mpv yt-dlp deno
+Write-Host "All installations complete!"
